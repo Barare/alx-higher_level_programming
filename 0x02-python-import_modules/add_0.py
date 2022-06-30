5ed7c482194a29b8dd7e9bed112c3a794d31a614
Barare <emmanuelbarare@gmail.com>
2022-06-30 16:59:08 +0300

    task zero

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,10 @@
+#!usr/bin/python3
+def add(a, b):
+    """My additton function
+    Args:
+    a: first integer
+    b: second integer
+    Returns:
+    The retrun value. a + b
+    """
+    return (a + b)
