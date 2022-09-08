e1996e9b3a96ac1f477fe62b29f11bafca493246
Barare <emmanuelbarare@gmail.com>
2022-09-08 11:16:19 +0300

    task four

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,10 @@
+-- creates a table called first_table in the current databse in your MySQL server.
+-- The database name will br passed as an argument of the mysql command
+-- fist_table description:
+--            id INT
+--            name VACHAR(256)
+-- The database name will be passed as an argument of the mysql command
+-- If the table first_table already exists, your script should not fail
+-- You are not allowed to use the SELECT of SHOW statements
+
+CREATE TABLE OF NOT EXISTS first_table (id INT, name VACHAR(256));
