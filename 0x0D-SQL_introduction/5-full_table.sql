631f5f262565e0fe80236a66bf2a09cd47990735
Barare <emmanuelbarare@gmail.com>
2022-09-08 11:21:23 +0300

    task five

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+-- prints the full description of the table firs_table from the databse hbtn_0c_0 in your MySQL server
+-- The databse name will be passed as an argument of the mysql command
+-- You are not allowed to use the SELECT or SHOW statements
+
+SHOW CREATE TABLE first_table;
